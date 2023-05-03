cd98cdad608bdb39041e39b7fa0196b12958f87e
Mathis Telschow <mathistelschow@gmail.com>
2023-05-03 10:21:36 +0200

    Change button style

--- components/banner/banner.tsx
+++ components/banner/banner.tsx
@@ -1,14 +1,24 @@
-import styles from '@/components/banner/banner.module.css'
+import styles from '@/components/banner/banner.module.css';
 
-export default function Banner({ buttonText, handleOnClick } : { buttonText: string, handleOnClick: any}){
+export default function Banner({
+	buttonText,
+	handleOnClick,
+}: {
+	buttonText: string;
+	handleOnClick: any;
+}) {
 	return (
 		<div className={styles.container}>
 			<h1 className={styles.title}>
-				<span className={styles.title1}>Coffee</span> 
-                <span className={styles.title2}>Connoisseur</span>
+				<span className={styles.title1}>Coffee</span>
+				<span className={styles.title2}>Connoisseur</span>
 			</h1>
 			<p className={styles.subTitle}>Discover you local coffe shops!</p>
-			<button className={styles.button} onClick={handleOnClick}>{buttonText}</button>
+			<div className={styles.buttonWrapper}>
+				<button className={styles.button} onClick={handleOnClick}>
+					{buttonText}
+				</button>
+			</div>
 		</div>
 	);
-};
+}
